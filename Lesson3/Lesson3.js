9093f5ff75587345bed3085227197a66a51f9688
Khanh Nguyen <khanhdaik134@gmail.com>
2020-09-25 06:19:52 +0700

    add image

---
+++ Lesson3/Lesson3.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Lesson3 = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default Lesson3
+
+const styles = StyleSheet.create({})
